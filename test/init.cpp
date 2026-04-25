f7be173367170091ffd73a339df41f704f077b3d
RanaAbdulMoeez <ranaabdulmoeez987@gmail.com>
2026-04-25 22:21:00 +0500

    Update test init.cpp: added comments

--- test/init.cpp
+++ test/init.cpp
@@ -5,10 +5,10 @@
 class FLatVectorStore
 {
 private:
-    int dimension;
-    std::vector<float> vectors;
+    int dimension; //dimension is fixed for all the vectors
+    std::vector<float> vectors; //all vectors are stored in 1d array for cache locality
     std::vector<long long> ids; //stores the id for the vector at ith pos
-    std::unordered_map<long long, long long> id_to_pos; //maps id to the pos of the vector
+    std::unordered_map<long long, long long> id_to_pos; //maps id to the pos of the vector for fast retrieval while checking existence of a particular id and overwriting
 public:
     FLatVectorStore(int dim) : dimension(dim) {}
     bool insert(long long id, const std::vector<float>& vec)
@@ -16,15 +16,15 @@ public:
         if (vec.size() != dimension)
             return false;
         auto it = id_to_pos.find(id);
-        if (it != id_to_pos.end())
+        if (it != id_to_pos.end()) //overwrite existing vector
         {
-            long long index = it->second * dimension;
+            long long index = it->second * dimension; //to calculate the starting index of the vector at ith pos
             for (int i = 0; i < dimension; i++)
             {
                 vectors[i+index] = vec[i];
             }
         }
-        else
+        else //inserting new vector
         {
             long long pos = ids.size();
             id_to_pos[id] = pos;
