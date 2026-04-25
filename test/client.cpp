16e5dafb23c6005afb78cddbe5b6dcf90041a56a
Abdul Raafey <abdulraafey12345@gmail.com>
2026-04-25 13:37:07 +0500

    test(network): Add comments and clean code

--- test/client.cpp
+++ test/client.cpp
@@ -1,28 +1,41 @@
-#include <iostream>
-#include <string>
-#include <cstring>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <unistd.h>
+#include <iostream>          // Basic Input/Output Library
+#include <cstring>           // For Character-Strings
+#include <netinet/in.h>      // Not complete sure yet
+#include <sys/socket.h>      // Not completely sure yet
+#include <unistd.h>          // Not completely sure yet
 using namespace std;
 
 int main(){
+    // Creating a client socket
     int ClientSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    // Initializing the Server Address and Type to connect to
     sockaddr_in ServerAddress;
     ServerAddress.sin_family = AF_INET;
     ServerAddress.sin_port = htons(8080);
     ServerAddress.sin_addr.s_addr = INADDR_ANY;
+
+    // Connect to the Server Address using the client socket we created
     connect(ClientSocket, (struct sockaddr*)& ServerAddress, sizeof(ServerAddress));
 
     while (true){
-        int check = 0;
+        // Holds the message to be sent to server, max 1024 char size
         char message[1024] = {0};
-        cout << message << " " << "2" << endl;
-        cin.get(message, 1024);
-        cin.ignore();
-        check = send(ClientSocket, message, strlen(message), 0);
+        cout << "Your message: ";
+        cin.get(message, 1024); cin.ignore();
+
+        // If no message, exit loop
+        if (strlen(message) == 0){
+            break;
+        }
+
+        // Send the message to server
+        send(ClientSocket, message, strlen(message), 0);
     }
 
+    cout << "Connection has been closed" << endl;
+
+    // Close the Client Socket
     close(ClientSocket);
     return 0;
 }
--- test/server.cpp
+++ test/server.cpp
@@ -1,38 +1,51 @@
-#include <iostream>
-#include <cstring>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <unistd.h>
+#include <iostream>          // Basic Input/Output Library
+#include <cstring>           // For Character-Strings
+#include <netinet/in.h>      // Not complete sure yet
+#include <sys/socket.h>      // Not completely sure yet
+#include <unistd.h>          // Not completely sure yet
 using namespace std;
 
 
 int main(){
-    cout << "Cow" << endl;
-    cout << "Buffalo" << endl;
 
+    // Creating a server socket
     int ServerSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    // Initializing the Server Address and Type
     sockaddr_in ServerAddress;
     ServerAddress.sin_family = AF_INET;
     ServerAddress.sin_port = htons(8080);
     ServerAddress.sin_addr.s_addr = INADDR_ANY;
 
+    // Binding the socket we created and the address we initialized
     bind(ServerSocket, (struct sockaddr*)& ServerAddress, sizeof(ServerAddress));
     cout << "Listening..." << endl;
+
+    // Waits/Listens for connections
     listen(ServerSocket, 5);
 
+    // Once a connection request arrives, accept it
+    // and store it in ClientSocket
     int ClientSocket = accept(ServerSocket, nullptr, nullptr);
     cout << "Connected with: " << ClientSocket << endl;
 
     while (true){
+        // Holds the message recieved from the Client, max 1024 char size
         char buffer[1024] = {0};
-        cout << "At the top" << endl;
+
+        // Recieves message from client
         recv(ClientSocket, buffer, sizeof(buffer), 0);
-        if (sizeof(buffer) == 0){
-            cout << "Unable to recieve messages" << endl;
+
+        // If no message, exit loop
+        if (strlen(buffer) == 0){
+            break;
         }
         cout << "Message from Client: " << buffer << endl;
     }
 
+    cout << "Connection has been closed" << endl;
+
+    // Close the ServersSocket
     close(ServerSocket);
     return 0;
 }
