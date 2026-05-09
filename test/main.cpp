b3735f22721cc8f54ff2085ad17c5a9e313e62d4
Ahmed <baca24070@itu.edu.pk>
2026-05-09 21:23:56 +0500

    test: add tester code in main

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,6 @@
 #include "FlatVectorStore.h"
 
-void print_vector(const std::vector<float>& v)
+void print_vector(const std::vector<float> &v)
 {
     for (int i = 0; i < v.size(); i++)
     {
@@ -9,16 +9,14 @@ void print_vector(const std::vector<float>& v)
     std::cout << std::endl;
 }
 
-
 int main()
 {
     FLatVectorStore v(4);
-    std::vector<float> v1 {0.1, 0.2, 0.3, 0.4};
-    std::vector<float> v2 {0.15, 0.25, 0.35, 0.45};
-    std::vector<float> v3 {0.9, 0.1, 0.1, 0.2};
-    std::vector<float> v4 {0.85, 0.15, 0.05, 0.25};
-    std::vector<float> v5 {0.5, 0.5, 0.5, 0.5};
-
+    std::vector<float> v1{0.1, 0.2, 75, 5};
+    std::vector<float> v2{0.15, 0.25, 0.35, 0.45};
+    std::vector<float> v3{81, 6, 12, 0.2};
+    std::vector<float> v4{0.85, 0.15, 15, 0.25};
+    std::vector<float> v5{10, 0.5, 8, 0.5};
 
     v.insert(1, v1);
     v.insert(2, v2);
@@ -30,7 +28,7 @@ int main()
 
     std::cout << std::endl;
 
-    std::vector<float> t {0.12, 0.22, 0.32, 0.42};
+    std::vector<float> t{0.12, 0.22, 0.32, 0.42};
     std::vector<std::vector<float>> k_vectors;
     std::vector<std::pair<long long, float>> ids_dis;
     v.k_nearest(3, t, k_vectors, ids_dis);
@@ -41,5 +39,18 @@ int main()
         print_vector(k_vectors[i]);
     }
 
+    cout << "\nRunning llyods algorithm..." << endl;
+
+    int k = 2;
+    vector<vector<float>> centroids = v.llyods_algorithm(k);
+
+    for (int i = 0; i < k; i++)
+    {
+        for (int j = 0; j < centroids[i].size(); j++)
+        {
+            cout << centroids[i][j] << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
